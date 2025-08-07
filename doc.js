19a3aa2e3cd7913809bbb65dbfd525f5507afa24
Pranjal-ux <pranjal2shukla@gmail.com>
2025-08-07 15:33:33 +0530

    #DOM

---
+++ doc.js
@@ -0,0 +1,9 @@
+let heading = document.getElementsByClassName("heading");
+console.dir(heading);
+console.log(heading);
+
+let element = document.querySelector("p");
+console.dir(element);
+let AllEl = document.querySelectorAll("p");
+console.dir(AllEl);
+
